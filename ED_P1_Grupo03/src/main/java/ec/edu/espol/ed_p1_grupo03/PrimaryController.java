087f34a7db018ec7ae01b425f64b5c6431654512
alan-am <alan.aguilardev@gmail.com>
2025-11-09 10:13:03 -0500

    Add base project

---
+++ ED_P1_Grupo03/src/main/java/ec/edu/espol/ed_p1_grupo03/PrimaryController.java
@@ -0,0 +1,12 @@
+package ec.edu.espol.ed_p1_grupo03;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ ED_P1_Grupo03/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module ec.edu.espol.ed_p1_grupo03 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens ec.edu.espol.ed_p1_grupo03 to javafx.fxml;
+    exports ec.edu.espol.ed_p1_grupo03;
+}
