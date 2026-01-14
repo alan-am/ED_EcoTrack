d2f82b1fb73a1ac137ec0f57ed729f8b0dc98a22
Ambar Salazar <ambasala@espol.edu.ec>
2026-01-14 14:37:20 -0500

    Punto 4 terminado con mejoras del punto 1

--- ED_P1_Grupo03/src/main/java/comparadores/ComparadorPorPrioridad.java
+++ ED_P1_Grupo03/src/main/java/comparadores/ComparadorPorPrioridad.java
@@ -13,8 +13,17 @@ import ec.edu.espol.ed_p1_grupo03.Residuo;
 
 public class ComparadorPorPrioridad implements Comparator<Residuo> {
 
+    private int valorPrioridad(String p) {
+        if (p == null) return 0;
+        switch (p.toLowerCase()) {
+            case "alta": return 1;
+            case "media": return 2;
+            case "baja": return 3;
+            default: return 0;
+        }
+    }
     @Override
     public int compare(Residuo r1, Residuo r2) {
-        return r1.getPrioridad().compareTo(r2.getPrioridad());
+        return Integer.compare(valorPrioridad(r1.getPrioridad()), valorPrioridad(r2.getPrioridad()));
     }
 }
